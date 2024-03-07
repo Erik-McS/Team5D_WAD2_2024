66ee1dfc53b8e3a03755e2ec63c443cab6963794
Ibrahim Asghar <2617847a@student.gla.ac.uk>
2024-03-07 19:58:30 +0000

    homepage mockup

--- ratearant/views.py
+++ ratearant/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
+from ratearant.models import Restaurant
 
 
 # Create your views here.
 
 
 def home(request):
-    context_dict = {'message': 'Placeholder for the Team 5D Rate-A-Rant homepage',
-                    'isHomePage': True}
+    restaurant_list = Restaurant.objects.order_by('-name')[:5]
+    restaurant_list2 = Restaurant.objects.order_by('name')[5:]
+
+    context_dict = {}
+    context_dict['top_message'] = 'Top Rated Restaurants'
+    context_dict['fave_message'] = 'Favourite Restaurants'
+    context_dict['restaurants'] = restaurant_list
+    context_dict['fave_restaurants'] = restaurant_list2
     return render(request, 'ratearant/home.html', context=context_dict)
 
 
