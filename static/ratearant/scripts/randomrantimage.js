3a3325e8bbab435528e00cfa9b22605125a27f6e
Erik McS <klaelfr60@outlook.com>
2024-03-16 21:13:48 +0000

    some work on the restaurant page

--- static/ratearant/scripts/randomrantimage.js
+++ static/ratearant/scripts/randomrantimage.js
@@ -1,3 +1,4 @@
+// function in JSQuery
 $(document).ready(function (){
     // store the path to the image
     var pathToImage = staticUrl + "rateArant_";
@@ -5,6 +6,6 @@ $(document).ready(function (){
     var nums=19;
     var random=Math.floor(Math.random()*nums)+1;
     var image=pathToImage+random+".jpg";
-    console.log(image);
+    //console.log(image);
     $('#restaurantpic').attr('src',image);
 });
